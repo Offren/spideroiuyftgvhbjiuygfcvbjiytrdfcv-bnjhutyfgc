d61bf9631a52117a6e5d7bfc89ad37196d761891
Offren <OffrenLLC@outlook.com>
2024-10-30 18:23:11 -0500

    Update settings.py

--- goodreads_scraper/settings.py
+++ goodreads_scraper/settings.py
@@ -3,13 +3,13 @@ BOT_NAME = 'goodreads_scraper'
 SPIDER_MODULES = ['goodreads_scraper.spiders']
 NEWSPIDER_MODULE = 'goodreads_scraper.spiders'
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# Don't obey robots.txt since we're respecting rate limits
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performing at the same time to the same domain
 CONCURRENT_REQUESTS_PER_DOMAIN = 1
 
-# Configure a delay for requests for the same website
+# Configure a delay for requests for the same website (3 seconds)
 DOWNLOAD_DELAY = 3
 
 # Enable or disable downloader middlewares
@@ -29,3 +29,21 @@ HTTPCACHE_EXPIRATION_SECS = 0
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
 HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# Add retry middleware settings
+RETRY_ENABLED = True
+RETRY_TIMES = 3
+RETRY_HTTP_CODES = [500, 502, 503, 504, 408, 429]
+
+# Add custom settings
+COOKIES_ENABLED = True
+DOWNLOAD_TIMEOUT = 180
+REDIRECT_ENABLED = True
+
+# Add request headers
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive'
+}
