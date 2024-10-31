ed7c8d35841bc7b9e308e02f0298c86e302afdb8
Offren <OffrenLLC@outlook.com>
2024-10-30 19:19:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='goodreads_scraper',
     version='1.0',
     packages=find_packages(),
+    include_package_data=True,
     package_data={
         'goodreads_scraper': ['data/*.csv'],
     },
-    entry_points={'scrapy': ['settings = goodreads_scraper.settings']},
     install_requires=[
         'scrapy>=2.11.0',
         'scrapy-user-agents>=0.1.1'
